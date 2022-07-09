00609a1969eca3c426b6a7515715ca82a76f7d95
Davis Kauffmann <daviskauffmann@gmail.com>
2022-07-09 13:11:47 -0400

    Add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.use(helmet());
 
   app.useGlobalPipes(
     new ValidationPipe({
